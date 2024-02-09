6a0adc8f3c7f7d762fc30aa268d122adf11f679a
Aaron Kagan <aaronkagan@yahoo.com>
2024-02-09 15:18:53 -0500

    adding preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,21 @@ import Confetti from 'react-confetti-boom';
 
 import { useDropzone } from 'react-dropzone';
 
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
 export default function Home() {
   const [imageURL, setImageURL] = useState('');
   const [loading, setLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
+  // const [preview, setPreview] = useState<any>('');
 
   const [img, setImg] = useState<any>('');
 
   const { toast } = useToast();
+
+  const router = useRouter();
 
   // function handleUploadFile(e) {
   //   const data = new FileReader();
@@ -82,57 +88,83 @@ export default function Home() {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <main className="flex gap-[2rem] flex-col items-center pt-[5rem]">
+    <main className="flex gap-[2rem] flex-col items-center ">
+      <nav>
+        <div
+          className="text-[40px] font-bold"
+          onClick={() => router.refresh()}
+        >
+          <span className="text-[blue]">zero</span>
+          <span className="">bg</span>
+        </div>
+      </nav>
       {/* <Input
         type="file"
         onChange={handleUploadFile}
         className="w-[250px]"
       /> */}
 
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <div className="border border-black border-dashed w-[500px] h-[150px] flex flex-col items-center gap-[1rem] justify-center bg-[#dcedf9]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48"
-              viewBox="0 0 48 48"
-            >
-              <path
-                d="M0 0h48v48h-48z"
-                fill="none"
-              />
-              <path d="M38.71 20.07c-1.36-6.88-7.43-12.07-14.71-12.07-5.78 0-10.79 3.28-13.3 8.07-6.01.65-10.7 5.74-10.7 11.93 0 6.63 5.37 12 12 12h26c5.52 0 10-4.48 10-10 0-5.28-4.11-9.56-9.29-9.93zm-10.71 5.93v8h-8v-8h-6l10-10 10 10h-6z" />
-            </svg>
-            <p>Drop the file here ...</p>
-          </div>
-        ) : (
-          <div className="border border-black border-dashed w-[500px] h-[150px] flex flex-col items-center gap-[1rem] justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48"
-              viewBox="0 0 48 48"
-            >
-              <path
-                d="M0 0h48v48h-48z"
-                fill="none"
-              />
-              <path d="M38.71 20.07c-1.36-6.88-7.43-12.07-14.71-12.07-5.78 0-10.79 3.28-13.3 8.07-6.01.65-10.7 5.74-10.7 11.93 0 6.63 5.37 12 12 12h26c5.52 0 10-4.48 10-10 0-5.28-4.11-9.56-9.29-9.93zm-10.71 5.93v8h-8v-8h-6l10-10 10 10h-6z" />
-            </svg>
-            <p>Drag &apos;n&apos; drop a file here, or click to select file</p>
-          </div>
-        )}
-      </div>
+      {!img && (
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <div className="border border-black border-dashed w-[500px] h-[150px] flex flex-col items-center gap-[1rem] justify-center bg-[#dcedf9]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="48"
+                height="48"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  d="M0 0h48v48h-48z"
+                  fill="none"
+                />
+                <path
+                  fill="blue"
+                  d="M38.71 20.07c-1.36-6.88-7.43-12.07-14.71-12.07-5.78 0-10.79 3.28-13.3 8.07-6.01.65-10.7 5.74-10.7 11.93 0 6.63 5.37 12 12 12h26c5.52 0 10-4.48 10-10 0-5.28-4.11-9.56-9.29-9.93zm-10.71 5.93v8h-8v-8h-6l10-10 10 10h-6z"
+                />
+              </svg>
+              <p>Drop the file here ...</p>
+            </div>
+          ) : (
+            <div className="border border-black border-dashed w-[500px] h-[150px] flex flex-col items-center gap-[1rem] justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="48"
+                height="48"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  d="M0 0h48v48h-48z"
+                  fill="none"
+                />
+                <path
+                  fill="blue"
+                  d="M38.71 20.07c-1.36-6.88-7.43-12.07-14.71-12.07-5.78 0-10.79 3.28-13.3 8.07-6.01.65-10.7 5.74-10.7 11.93 0 6.63 5.37 12 12 12h26c5.52 0 10-4.48 10-10 0-5.28-4.11-9.56-9.29-9.93zm-10.71 5.93v8h-8v-8h-6l10-10 10 10h-6z"
+                />
+              </svg>
+              <p>
+                Drag &apos;n&apos; drop a file here, or click to select file
+              </p>
+            </div>
+          )}
+        </div>
+      )}
 
-      {img && (
-        <Button
-          variant="outline"
-          onClick={fetchImageURL}
-        >
-          Remove Background
-        </Button>
+      {img && !isSuccess && (
+        <>
+          <Image
+            src={img}
+            width={200}
+            height={200}
+          />
+          <Button
+            variant="outline"
+            onClick={fetchImageURL}
+          >
+            Remove Background
+          </Button>
+        </>
       )}
 
       {imageURL && (
