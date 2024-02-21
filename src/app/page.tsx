a00bd7e58ab2e7c7dee2f0cfa5ff85ae8f4269ae
Aaron Kagan <aaronkagan@yahoo.com>
2024-02-21 14:19:33 -0500

    moving toast outside of main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
-  const [img, setImg] = useState<any>('');
+  const [img, setImg] = useState('');
 
   const { toast } = useToast();
 
@@ -49,7 +49,7 @@ export default function Home() {
       toast({
         variant: 'destructive',
         title: 'Unexpected Error',
-        description: 'Please Try Again or Try A Different Image',
+        description: `Please Try Again or Try A Different Image`,
         className: 'bg-[red] text-[white]'
       });
       setIsError(true);
@@ -81,223 +81,225 @@ export default function Home() {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <main className="flex gap-[2rem] flex-col items-center  min-h-[100vh] text-center max-w-[95%]">
-      <nav className="flex w-[90%] justify-between items-center">
-        <div
-          className="text-[40px] font-bold cursor-pointer"
-          onClick={() => location.reload()}
-        >
-          <span className="text-[blue]">zero</span>
-          <span className="">bg</span>
-        </div>
-      </nav>
-
-      <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-        Remove backgrounds from images with a click
-      </h1>
-      <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed dark:text-gray-400">
-        Easily cut out the background of any photo using our AI-powered tool. No
-        design skills required.
-      </p>
-
-      {!img && (
-        <div
-          className="flex justify-center cursor-pointer"
-          {...getRootProps()}
-        >
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <div className="border border-gray-400 border-dashed w-[300px] md:w-[400px] max-w-[90%] h-[150px] flex flex-col items-center gap-[1rem]   bg-[#dcedf9] rounded-[8px] p-3">
-              <div className="h-[50%] flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="48"
-                  height="48"
-                  viewBox="0 0 48 48"
-                >
-                  <path
-                    d="M0 0h48v48h-48z"
-                    fill="none"
-                  />
-                  <path
-                    fill="blue"
-                    d="M38.71 20.07c-1.36-6.88-7.43-12.07-14.71-12.07-5.78 0-10.79 3.28-13.3 8.07-6.01.65-10.7 5.74-10.7 11.93 0 6.63 5.37 12 12 12h26c5.52 0 10-4.48 10-10 0-5.28-4.11-9.56-9.29-9.93zm-10.71 5.93v8h-8v-8h-6l10-10 10 10h-6z"
-                  />
-                </svg>
-              </div>
-
-              <p className="text-gray-500">Drop image here ...</p>
-            </div>
-          ) : (
-            <div className="border border-gray-400 border-dashed w-[300px] md:w-[400px] max-w-[90%] h-[150px] flex flex-col items-center gap-[1rem] rounded-[8px] p-3">
-              <div className="h-[50%] flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="48"
-                  height="48"
-                  viewBox="0 0 48 48"
-                >
-                  <path
-                    d="M0 0h48v48h-48z"
-                    fill="none"
-                  />
-                  <path
-                    fill="blue"
-                    d="M38.71 20.07c-1.36-6.88-7.43-12.07-14.71-12.07-5.78 0-10.79 3.28-13.3 8.07-6.01.65-10.7 5.74-10.7 11.93 0 6.63 5.37 12 12 12h26c5.52 0 10-4.48 10-10 0-5.28-4.11-9.56-9.29-9.93zm-10.71 5.93v8h-8v-8h-6l10-10 10 10h-6z"
-                  />
-                </svg>
-              </div>
-
-              <p className="text-gray-500">
-                Drag &apos;n&apos; drop an image here, or click to select an
-                image
-              </p>
-            </div>
-          )}
-        </div>
-      )}
-
-      {img && !loading && !isSuccess && (
-        <>
-          <Image
-            alt="uploaded image"
-            src={img}
-            width={200}
-            height={200}
-            className=" rounded-[8px] h-[100%] w-[300px] md:w-[400px] max-w-[90%]"
-          />
-
-          <Button
-            variant="outline"
-            onClick={fetchImageURL}
-          >
-            Remove Background
-          </Button>
-        </>
-      )}
-
-      {loading && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 200 200"
-          className="w-[300px] h-[40px] flex items-center mt-10"
-        >
-          <circle
-            fill="#9CA3AF"
-            stroke="#9CA3AF"
-            strokeWidth="1"
-            r="70"
-            cx="-200"
-            cy="100"
-          >
-            <animate
-              attributeName="opacity"
-              calcMode="spline"
-              dur="2"
-              values="1;0;1;"
-              keySplines=".5 0 .5 1;.5 0 .5 1"
-              repeatCount="indefinite"
-              begin="-.4"
-            ></animate>
-          </circle>
-          <circle
-            fill="#9CA3AF"
-            stroke="#9CA3AF"
-            strokeWidth="1"
-            r="70"
-            cx="100"
-            cy="100"
-          >
-            <animate
-              attributeName="opacity"
-              calcMode="spline"
-              dur="2"
-              values="1;0;1;"
-              keySplines=".5 0 .5 1;.5 0 .5 1"
-              repeatCount="indefinite"
-              begin="-.2"
-            ></animate>
-          </circle>
-          <circle
-            fill="#9CA3AF"
-            stroke="#9CA3AF"
-            strokeWidth="1"
-            r="70"
-            cx="400"
-            cy="100"
-          >
-            <animate
-              attributeName="opacity"
-              calcMode="spline"
-              dur="2"
-              values="1;0;1;"
-              keySplines=".5 0 .5 1;.5 0 .5 1"
-              repeatCount="indefinite"
-              begin="0"
-            ></animate>
-          </circle>
-        </svg>
-      )}
-      {imageURL && (
-        <div className=" w-[300px] md:w-[400px] max-w-[90%] border-black border rounded-[8px] ">
-          <ReactCompareImage
-            leftImage={img}
-            rightImage={imageURL}
-          />
-        </div>
-      )}
-
-      {imageURL && (
-        <Button
-          variant="outline"
-          onClick={downloadImage}
-        >
-          Download Image
-        </Button>
-      )}
-
-      {img && !loading && (
-        <Button
-          variant="outline"
-          onClick={handleReset}
-        >
-          Process A Different Image
-        </Button>
-      )}
-
+    <>
       {isError && <Toaster />}
-      {isSuccess && (
-        <Confetti
-          particleCount={200}
-          effectInterval={1000}
-          effectCount={3}
-          spreadDeg={360}
-          mode="boom"
-          y={0.2}
-        />
-      )}
-      <footer className="flex flex-col items-center justify-center gap-2 py-4 mt-auto mb-0 text-sm font-medium">
-        <p className="text-gray-500">
-          Designed and Developed by{' '}
-          <a
-            className="text-[black]"
-            href="https://www.aaronkagan.dev/"
+      <main className="flex gap-[2rem] flex-col items-center  min-h-[100vh] text-center max-w-[95%]">
+        <nav className="flex w-[90%] justify-between items-center">
+          <div
+            className="text-[40px] font-bold cursor-pointer"
+            onClick={() => location.reload()}
           >
-            Aaron Kagan
-          </a>
+            <span className="text-[blue]">zero</span>
+            <span className="">bg</span>
+          </div>
+        </nav>
+
+        <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+          Remove backgrounds from images with a click
+        </h1>
+        <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed dark:text-gray-400">
+          Easily cut out the background of any photo using our AI-powered tool.
+          No design skills required.
         </p>
 
-        <div className="flex gap-3">
-          <p className="text-[black]">
-            <a href="https://github.com/aaronkagan/zerobg">Github Repo</a>
-          </p>
-          <p className="text-[black]">
-            <a href="https://www.linkedin.com/in/aaron-kagan/">LinkedIn</a>
-          </p>
-        </div>
+        {!img && (
+          <div
+            className="flex justify-center cursor-pointer"
+            {...getRootProps()}
+          >
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <div className="border border-gray-400 border-dashed w-[300px] md:w-[400px] max-w-[90%] h-[150px] flex flex-col items-center gap-[1rem]   bg-[#dcedf9] rounded-[8px] p-3">
+                <div className="h-[50%] flex items-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="48"
+                    height="48"
+                    viewBox="0 0 48 48"
+                  >
+                    <path
+                      d="M0 0h48v48h-48z"
+                      fill="none"
+                    />
+                    <path
+                      fill="blue"
+                      d="M38.71 20.07c-1.36-6.88-7.43-12.07-14.71-12.07-5.78 0-10.79 3.28-13.3 8.07-6.01.65-10.7 5.74-10.7 11.93 0 6.63 5.37 12 12 12h26c5.52 0 10-4.48 10-10 0-5.28-4.11-9.56-9.29-9.93zm-10.71 5.93v8h-8v-8h-6l10-10 10 10h-6z"
+                    />
+                  </svg>
+                </div>
 
-        <p className="text-gray-500">© 2024 ZeroBG. All rights reserved.</p>
-      </footer>
-    </main>
+                <p className="text-gray-500">Drop image here ...</p>
+              </div>
+            ) : (
+              <div className="border border-gray-400 border-dashed w-[300px] md:w-[400px] max-w-[90%] h-[150px] flex flex-col items-center gap-[1rem] rounded-[8px] p-3">
+                <div className="h-[50%] flex items-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="48"
+                    height="48"
+                    viewBox="0 0 48 48"
+                  >
+                    <path
+                      d="M0 0h48v48h-48z"
+                      fill="none"
+                    />
+                    <path
+                      fill="blue"
+                      d="M38.71 20.07c-1.36-6.88-7.43-12.07-14.71-12.07-5.78 0-10.79 3.28-13.3 8.07-6.01.65-10.7 5.74-10.7 11.93 0 6.63 5.37 12 12 12h26c5.52 0 10-4.48 10-10 0-5.28-4.11-9.56-9.29-9.93zm-10.71 5.93v8h-8v-8h-6l10-10 10 10h-6z"
+                    />
+                  </svg>
+                </div>
+
+                <p className="text-gray-500">
+                  Drag &apos;n&apos; drop an image here, or click to select an
+                  image
+                </p>
+              </div>
+            )}
+          </div>
+        )}
+
+        {img && !loading && !isSuccess && (
+          <>
+            <Image
+              alt="uploaded image"
+              src={img}
+              width={200}
+              height={200}
+              className=" rounded-[8px] h-[100%] w-[300px] md:w-[400px] max-w-[90%]"
+            />
+
+            <Button
+              variant="outline"
+              onClick={fetchImageURL}
+            >
+              Remove Background
+            </Button>
+          </>
+        )}
+
+        {loading && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 200 200"
+            className="w-[300px] h-[40px] flex items-center mt-10"
+          >
+            <circle
+              fill="#9CA3AF"
+              stroke="#9CA3AF"
+              strokeWidth="1"
+              r="70"
+              cx="-200"
+              cy="100"
+            >
+              <animate
+                attributeName="opacity"
+                calcMode="spline"
+                dur="2"
+                values="1;0;1;"
+                keySplines=".5 0 .5 1;.5 0 .5 1"
+                repeatCount="indefinite"
+                begin="-.4"
+              ></animate>
+            </circle>
+            <circle
+              fill="#9CA3AF"
+              stroke="#9CA3AF"
+              strokeWidth="1"
+              r="70"
+              cx="100"
+              cy="100"
+            >
+              <animate
+                attributeName="opacity"
+                calcMode="spline"
+                dur="2"
+                values="1;0;1;"
+                keySplines=".5 0 .5 1;.5 0 .5 1"
+                repeatCount="indefinite"
+                begin="-.2"
+              ></animate>
+            </circle>
+            <circle
+              fill="#9CA3AF"
+              stroke="#9CA3AF"
+              strokeWidth="1"
+              r="70"
+              cx="400"
+              cy="100"
+            >
+              <animate
+                attributeName="opacity"
+                calcMode="spline"
+                dur="2"
+                values="1;0;1;"
+                keySplines=".5 0 .5 1;.5 0 .5 1"
+                repeatCount="indefinite"
+                begin="0"
+              ></animate>
+            </circle>
+          </svg>
+        )}
+        {imageURL && (
+          <div className=" w-[300px] md:w-[400px] max-w-[90%] border-black border rounded-[8px] ">
+            <ReactCompareImage
+              leftImage={img}
+              rightImage={imageURL}
+            />
+          </div>
+        )}
+
+        {imageURL && (
+          <Button
+            variant="outline"
+            onClick={downloadImage}
+          >
+            Download Image
+          </Button>
+        )}
+
+        {img && !loading && (
+          <Button
+            variant="outline"
+            onClick={handleReset}
+          >
+            Process A Different Image
+          </Button>
+        )}
+
+        {isSuccess && (
+          <Confetti
+            particleCount={200}
+            effectInterval={1000}
+            effectCount={3}
+            spreadDeg={360}
+            mode="boom"
+            y={0.2}
+          />
+        )}
+        <footer className="flex flex-col items-center justify-center gap-2 py-4 mt-auto mb-0 text-sm font-medium">
+          <p className="text-gray-500">
+            Designed and Developed by{' '}
+            <a
+              className="text-[black]"
+              href="https://www.aaronkagan.dev/"
+            >
+              Aaron Kagan
+            </a>
+          </p>
+
+          <div className="flex gap-3">
+            <p className="text-[black]">
+              <a href="https://github.com/aaronkagan/zerobg">Github Repo</a>
+            </p>
+            <p className="text-[black]">
+              <a href="https://www.linkedin.com/in/aaron-kagan/">LinkedIn</a>
+            </p>
+          </div>
+
+          <p className="text-gray-500">© 2024 ZeroBG. All rights reserved.</p>
+        </footer>
+      </main>
+    </>
   );
 }
